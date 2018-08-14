950e6960f534c870c22065301e79d0f4f9ac9bf1
richkmtb <36201949+richkmtb@users.noreply.github.com>
2018-08-14 17:49:41 +0100

    Update

--- script.js
+++ script.js
@@ -8,9 +8,16 @@ function inputLength() {
 
 function createListElement() {
 	var li = document.createElement("li");
+	li.classList.add("listItem");
+	li.addEventListener("click", markListItem)
 	li.appendChild(document.createTextNode(input.value));
 	ul.appendChild(li);
 	input.value = "";
+
+
+	function markListItem() {
+		li.classList.toggle("done");
+	}
 }
 
 function addListAfterClick() {
